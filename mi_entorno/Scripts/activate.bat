4751456635d081854978e27c54a23f56df7e7a42
MatiasAFernandez <matiasafernandez23@gmail.com>
2025-10-01 10:44:52 -0300

    v2.0

--- mi_entorno/Scripts/activate.bat
+++ mi_entorno/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Usuario\Desktop\Micro Repo\mi_entorno"
+set "VIRTUAL_ENV=C:\Users\Usuario\Desktop\UI_Micro\mi_entorno"
 
 if not defined PROMPT set PROMPT=$P$G
 
